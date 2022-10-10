aa2fa969f1061c300e9f9909074f87b9d353db9f
nwebpro <nwebpro.work@gmail.com>
2022-10-11 03:02:56 +0600

    show currect ans func added

--- src/components/QuizAnswer.jsx
+++ src/components/QuizAnswer.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizAnswer = ({qa}) => {
     const {question, options, correctAnswer} = qa;
 
     const correctAnswers = (option) => {
         if(option === correctAnswer){
-            // alert('hello');
+            toast.success('Your answer is correct!', {autoClose: 500});
         }else{
-            // alert('wrong!');
+            toast.error('Your answer is incorrect!', {autoClose: 500});
         }
     }
+
+    const showCorrectAnswer = () => {
+        toast.success(`${correctAnswer}`);
+    }
+
     return (
-        <div className='max-w-xl sm:mx-auto lg:max-w-2xl mb-5 bg-[#f1f1f1] p-10 rounded'>
+        <div className='max-w-xl sm:mx-auto lg:max-w-2xl mb-5 bg-[#f1f1f1] p-10 rounded relative'>
+            <ToastContainer position="top-center" autoClose={1000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+            <div className='absolute right-3 top-3'>
+                <button onClick={showCorrectAnswer}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </button>
+
+            </div>
             <div>
                 <h1 className='text-center mb-5'>{question}</h1>
                 <div>
--- src/components/QuizTest.jsx
+++ src/components/QuizTest.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import QuizAwnser from './QuizAnswer';
+import QuizAnswer from './QuizAnswer';
 
 const QuizTest = () => {
     const quizTestData = useLoaderData();
     const quizTest = quizTestData.data.questions;
     return (
-        <div className='my-20'>
+        <div className='my-20 px-5 lg:px-0'>
             {
                 quizTest.map(qa => (
-                    <QuizAwnser key={qa.id} qa={qa} />
+                    <QuizAnswer key={qa.id} qa={qa} />
                 ))
             }
         </div>
