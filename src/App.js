7995cfd73736b9a879fb52142165471e784047bf
nwebpro <nwebpro.work@gmail.com>
2022-10-10 23:00:02 +0600

    Hero component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
     return (
         <div>
-            <Header />
+            <Home />
         </div>
     );
 };
